6f95880852c9629cbb64ba861da4e5518d7a71ee
Pavel <zorin.pavel@gmail.com>
2016-03-22 10:36:54 +0300

    Theme

--- app/src/main/java/ru/bobsoccer/MainActivity.java
+++ app/src/main/java/ru/bobsoccer/MainActivity.java
@@ -2,34 +2,75 @@ package ru.bobsoccer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@SuppressWarnings("unchecked")
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
+    private Session activitySession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activitySession = new Session(this);
+        TextView tokenView = (TextView) findViewById(R.id.tokenView);
+        tokenView.setText(String.valueOf(Session.Token));
+    }
+
+    public void buttonClick(View view) {
         Map<String, String> params = new HashMap<>();
         params.put("paramKey", "paramValue");
         new API(this, new API.ApiResponse() {
             @Override
-            public void onTaskCompleted(JSONObject jsonObj) {
-                Log.d(TAG, String.valueOf(jsonObj));
+            public void onTaskCompleted(JSONObject resultObj) {
+                TextView tView = (TextView) findViewById(R.id.textView);
+                JSONObject User;
+                try {
+                    User = resultObj.getJSONObject("User");
+                    tView.setText(String.valueOf(User));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        }, "GET", "Users", "GetUser").execute(params);
+        }, "GET", "Users", "anyFunc").execute(params);
     }
 
-    public void buttonClick(View view) {
+    public void setuserClick(View view) {
+        Map<String, String> params = new HashMap<>();
+        params.put("login", "admin");
+        params.put("pass", "5awwor");
+        new API(this, new API.ApiResponse() {
+            @Override
+            public void onTaskCompleted(JSONObject resultObj) {
+                TextView tView = (TextView) findViewById(R.id.textView);
+                JSONObject User;
+                try {
+                    String apiToken = resultObj.getString("Token");
+                    activitySession.SetToken(apiToken);
+                    TextView tokenView = (TextView) findViewById(R.id.tokenView);
+                    tokenView.setText(String.valueOf(Session.Token));
+
+                    User = resultObj.getJSONObject("User");
+                    tView.setText(String.valueOf(User));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "GET", "Users", "CheckValidEnter").execute(params);
     }
 
 }
